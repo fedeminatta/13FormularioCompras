c14219cae0dacf61af443547d2a76a802e34a8ce
fedeminatta <minattafederico@gmail.com>
2023-01-25 20:02:28 -0300

    commit

---
+++ 1.js
@@ -0,0 +1,44 @@
+const inputText = document.querySelector('#text');
+const inputUnidades = document.querySelector('#text-unidades');
+const inputPrecio = document.querySelector('#text-precio');
+const enviar = document.querySelector('#enviar');
+const form = document.querySelector('#form');
+
+function insertarFila() {
+	let tblDatos = document.querySelector('#tblDatos').insertRow(1);
+	let col1 = tblDatos.insertCell(0);
+	let col2 = tblDatos.insertCell(1);
+	let col3 = tblDatos.insertCell(2);
+	let col4 = tblDatos.insertCell(3);
+
+	col1.innerHTML = inputText.value;
+	col2.innerHTML = inputPrecio.value;
+	col3.innerHTML = inputUnidades.value;
+	resultado = parseInt(inputPrecio.value) * parseInt(inputUnidades.value);
+	col4.innerHTML = Number(resultado);
+}
+
+function sumUnidades() {
+	let total = 0;
+	const tabla = document.getElementById('tblDatos');
+	for (let i = 1; i < tabla.rows.length - 1; i++) {
+		let rowValue = tabla.rows[i].cells[2].innerHTML;
+		total = total + Number(rowValue);
+	}
+	const tdTotal = document.getElementById('unidades');
+	tdTotal.textContent = total;
+}
+function sumTotal() {
+	let total = 0;
+	const tabla = document.getElementById('tblDatos');
+	for (let i = 1; i < tabla.rows.length - 1; i++) {
+		let rowValue = tabla.rows[i].cells[3].innerHTML;
+		total = total + Number(rowValue);
+	}
+	const tdTotal = document.getElementById('total');
+	tdTotal.textContent = total;
+}
+
+function resetearForm() {
+	form.reset();
+}
